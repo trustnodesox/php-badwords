9c540a84f14267ee17de9f101ef9c74da7f9784e
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-04-28 18:13:35 +0200

    completo

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
+
+
 <?php
-$testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+// Creare una variabile con un paragrafo di testo. Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con ***
+$testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+var_dump($testo);
+strlen($testo);
  ?>
 
-<div class="">
-  <?php echo $testo ?>
+<div class="" style="margin-top: 100px;">
+  <?php
+  echo str_replace($_GET['badword'], '***', $testo);
+  ?>
 </div>
